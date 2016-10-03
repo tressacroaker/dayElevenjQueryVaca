f4b6cb0dda5b13449d27f66de0a723a805e44aa1
tressacroaker <tressacroaker36@gmail.com>
2016-10-03 11:39:49 -0600

    updated

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('button').on('click', function() {
+    var message = $('<span>Call 1-800-888-0000 to book this tour</span>');
+    $(this).append(message);
+    $(this).remove();
+  });
+
+
+
+
+
+
+
+
+
+
+  });
