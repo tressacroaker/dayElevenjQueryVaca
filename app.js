baba12b87d5391fbb883a910b4a7b6a91a51b744
tressacroaker <tressacroaker36@gmail.com>
2016-10-04 01:15:38 -0600

    basic layout

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 $(document).ready(function(){
-  $('button').on('click', function() {
-    var message = $('<span>Call 1-800-888-0000 to book this tour</span>');
-    $(this).append(message);
-    $(this).remove();
+ $("h1").css({"font-family": "Cutive Mono", "font-size": "60px"});
+ $("p").css({"font-family": "Josefin Sans", "font-size": "20px"});
+
+
+
+ $("h1").hover(function(){
+   $("p").fadeIn();
   });
 
 
+  $(".menu").on("click", function(){
+      $(".menudropdown").toggle();
+    });
 
 
+  $("#plusdrink").on("click", function(){
+      $(".drinkdropdown").slideToggle();
+    });
 
+  $("#plustype").on("click", function(){
+      $(".typedropdown").slideToggle();
+    });
 
+  $("#pluslisten").on("click", function(){
+      $(".listendropdown").slideToggle();
+    });
 
+});
 
+  $("#coffee").hover(function(){
+    $("#coffee").fadeTo("slow", 0.4, function(){
+      });
+    });
 
+  $("#typewriter").hover(function(){
+    $("#typewriter").fadeTo("slow", 0.4, function(){
+      });
+    });
 
-  });
+  $("#music").hover(function(){
+    $("#music").fadeTo("slow", 0.4, function(){
+      });
+    });
